062f101e358c1dbfe8c57dc56f64fd8423bc169b
Hykilpikonna <admin@moecraft.cc>
2018-03-07 22:32:39 -0500

    0.0.0.0000 Template Commit
    Just imported the templates xD
    导入模板_(:з」∠)_

---
+++ src/cc/moecraft/saukiya/bukkitforgeitemslib/Main.java
@@ -0,0 +1,33 @@
+package cc.moecraft.saukiya.bukkitforgeitemslib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/03/07 创建!
+ * Created by Hykilpikonna on 2018/03/07!
+ * Twitter: @Hykilpikonna
+ * QQ/Wechat: 871674895
+ */
+public class Main extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        // 模板
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // 模板
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void onLoad()
+    {
+        // 模板
+        throw new UnsupportedOperationException();
+    }
+}
